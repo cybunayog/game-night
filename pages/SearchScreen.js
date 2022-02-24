6577b16cadfc22ede72f0ee1cd91e5b4e121ebab
Dasani Thompson <dthompson18@apu.edu>
2022-02-23 19:09:47 -0800

    added markers to search screen

--- pages/SearchScreen.js
+++ pages/SearchScreen.js
@@ -1,14 +1,20 @@
-import * as React from 'react';
-//import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
-import { PROVIDER_GOOGLE } from "react-native-maps";
-import MapView, { Marker, AnimatedRegion } from 'react-native-maps';
+import React from 'react'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import Paragraph from '../components/Paragraph'
+import { StyleSheet, Text, View, Dimensions, Image } from 'react-native'
+import MapView, { Marker, AnimatedRegion } from 'react-native-maps'
+import { PROVIDER_GOOGLE } from "react-native-maps"
 
-export default function SearchScreen() {
+export default function SearchScreen({ navigation }) {
     return (
-        <View style={styles.container}>
+     //  View style={styles.container}>
             <MapView
+            
                 style={styles.map}
+                loadingEnabled={true}
                 provider={PROVIDER_GOOGLE}
                 initialRegion={{
                     latitude: 34.133547,
@@ -20,31 +26,65 @@ export default function SearchScreen() {
                 zoomEnabled={true}
                 scrollEnabled={true}
                 showsScale={true}
+                 
             >
                 <Marker
                     coordinate={{
-                        latitude: 34.133547,
+                        latitude: 34.133547, // Azusa
                         longitude: -117.907542,
                     }}
-                    description={"This is a marker in React Natve"}
+                    title={"You Are Here!"}
                 >
 
-                    <Image source={require('./assets/gameNight_marker.png')} style={{ height: 100, width: 100 }} />
+                    <Image source={require('./assets/gameNight_markerBlack.png')} style={{ height: 75, width: 75 }} />
                 </Marker>
-            </MapView>
+                <Marker
+                    coordinate={{
+                        latitude: 34.12870314, // starbucks
+                        longitude: -117.8836472,
+                    }}
+                  //  description={"Monopoly"}
+       
+                >
+        <MapView.Callout>
+        <View style={{height: 80, width: 200}}>
+          <Text styles={styles.paragraph}> Monopoly </Text>
+          <Text> StarBucks, Foothill Ave.</Text>
+          <Text> Host: Danielle Dominguez</Text>
+          <Text> March 14, 2022 </Text>
         </View>
-    );
-}
+      </MapView.Callout>
+                    <Image source={require('./assets/gameNight_marker.png')} style={{ height: 70, width: 70 }} />
+                </Marker>
+              <Marker
+                    coordinate={{ // jp coffee shop
+                        latitude: 34.1280, 
+                        longitude: -117.8496,
+                    }}
+                    description={"Life"}
+                >
 
+                    <Image source={require('./assets/gameNight_marker.png')} style={{ height: 70, width: 70 }} />
+                </Marker>
+               
+           </MapView>
+        
+    )
+}
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    map: {
+  image: {
+    width: 430,
+    height: 760,
+    marginBottom: 10,
+  },
+   map: {
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height,
     },
-});
+    paragraph: {
+    margin: 24,
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
+})
